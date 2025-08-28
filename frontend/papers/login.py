2a48aef7e4e21ae52f7113339c300ead923562e7
Dileep Naidu <dileepnaiduone@gmail.com>
2025-08-28 17:30:50 +0530

    Welome page done

--- frontend/papers/login.py
+++ frontend/papers/login.py
@@ -2,18 +2,22 @@ import streamlit as st
 with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
-c0, c1, c2, c3= st.columns([0.1, 0.5, 0.3, 0.1])
+c0, c1, c2, c3, c4= st.columns([0.1, 0.45, 0.1, 0.25, 0.1])
 
 with c1:
     with st.container(key="custom1"):
         with st.container(key="customLogo"):
-            st.image(r"frontend\assets\STAR.png")
+            s0, s1, = st.columns([0.2, 0.8])
+            with s0:
+                st.image(r"frontend\assets\STAR.png")
         with st.container(key="customHeadLine"):
-            st.text("HeadLine")
+            st.text("A Personalized Guide\nto Your Future")
+        with st.container(key="customHeadTags"):
+            st.markdown("""Résumé Feedback &nbsp;&nbsp;•&nbsp;&nbsp; Career Guide &nbsp;&nbsp;•&nbsp;&nbsp; Role Judge &nbsp;&nbsp;•&nbsp;&nbsp; Step-by-Step Skill Path &nbsp;&nbsp;•&nbsp;&nbsp; A Friend Who Believes in You""")
         with st.container(key="customLogin"):
             st.pills(label="select", options=["Sign Up", "Login"], label_visibility="collapsed")
 
 
 
-with c2:
+with c3:
     st.image(r"frontend\assets\Home-Stick.png")
