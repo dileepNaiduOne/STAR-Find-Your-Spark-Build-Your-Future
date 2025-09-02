fc2ebe1ff66b471f1442e910761406e922c6ee56
Dileep Naidu <dileepnaiduone@gmail.com>
2025-09-02 22:08:02 +0530

    Added scacy error handling

--- backend/code/functions.py
+++ backend/code/functions.py
@@ -16,8 +16,8 @@ import spacy
 # import subprocess
 # import sys
 # subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-from spacy.cli import download
-download("en_core_web_sm")
+# from spacy.cli import download
+# download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
 
 
